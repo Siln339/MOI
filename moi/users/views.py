35db3e3cbb8787390fac6cd7fb33d83e2e1e0536
FreezeGamer96 <ilikesupersu@gmail.com>
2024-05-08 21:49:51 +0500

    Registration page view created

--- moi/users/views.py
+++ moi/users/views.py
@@ -1,9 +1,17 @@
+from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
-from .forms import AuthorizationForm
+from .forms import AuthorizationForm, RegistrationForm
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 class AuthorizationView(LoginView):
     form_class = AuthorizationForm
-    next_page = reverse_lazy('login')
+    next_page = reverse_lazy('')
     template_name = 'users/authorization.html'
     title = 'Вход'
+
+class RegistrationView(CreateView):
+    form_class = RegistrationForm
+    success_url = reverse_lazy('')
+    template_name = 'users/registration.html'
+    title = 'Регистрация'
