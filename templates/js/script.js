7d428f6c07caa6bf573fad9089777c11282a1f93
ViktorM13 <rendalakjewkyle@gmail.com>
2024-06-14 08:56:58 +0500

    Second animation for route container added

--- templates/js/script.js
+++ templates/js/script.js
@@ -3,12 +3,25 @@ const route_container = document.querySelector('.route-container');
 let parity_counter = 1;
 function route_window_toggle(){
     if (parity_counter % 2 != 0){
-        route_container.classList.add('active');
+        route_container.classList.add('active')
+        // route_container.classList.add('fade-out');
         parity_counter += 1;
     }
     else { 
-        route_container.classList.remove('active');
+        route_container.classList.remove('active')
+        // route_container.classList.remove('fade-out');
+        // route_container.classList.add('fade-in');
         parity_counter += 1;
     }
 }
 button.addEventListener('click', route_window_toggle);
+
+const submit_button = document.querySelector('.submit-field');
+const propeties_container = document.querySelector('.propeties-container');
+
+// function route_window_toggle(submit_button){
+//     if (data-routeid == '21'){
+//         route_container.classList.add('active')
+//         parity_counter += 1;
+//     }
+// }
