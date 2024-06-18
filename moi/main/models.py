dbe2ba35e42c39642707297b92c805aae7a4a4db
FreezeGamer96 <ilikesupersu@gmail.com>
2024-06-18 13:51:51 +0500

    Route model updated
    - Updated data types for description, difficult and length fields
    - Difficulty is now a selection field
    - Folder for saving images and gpx files of routes has been changed
    - Added __str__ method

--- moi/main/models.py
+++ moi/main/models.py
@@ -14,10 +14,13 @@ class Type(models.Model):
 
 class Route(models.Model):
     name = models.CharField(max_length=40)
-    descripion = models.CharField(max_length=300)
-    difficult = models.IntegerField()
-    lenght = models.IntegerField()
-    main_image = models.ImageField(upload_to='route_images/additional/')
-    gpx = models.FileField(upload_to='route_gpx/', max_length=100)
+    descripion = models.TextField(max_length=1000)
+    difficult = models.PositiveIntegerField(default=1, choices=((i,i) for i in range(1, 10)))
+    lenght = models.PositiveIntegerField()
+    main_image = models.ImageField(upload_to='main/routes_files/images/additional/')
+    gpx = models.FileField(upload_to='main/routes_files/gpx/', max_length=100)
     type = models.ForeignKey(Type, on_delete=models.PROTECT) #Поставить on_delete=models.SET_DEFAULT
     additional_images = models.ManyToManyField(Image)
+    
+    def __str__(self):
+        return self.name
