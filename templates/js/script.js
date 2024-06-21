8d75d37b6b0b2f550fbdc8ac9d8e9de3b4404666
ViktorM13 <rendalakjewkyle@gmail.com>
2024-06-22 03:24:41 +0500

    Added a slider for the route description block

--- templates/js/script.js
+++ templates/js/script.js
@@ -38,7 +38,29 @@ function swapContainer() {
     });
 }
 
-function getRouteData {
-    
-}
+// function getRouteData {
+
+// }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
